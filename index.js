d83a89f080afa253f56a562d1224781f46e78198
aaronespinoza <aarone@berkeley.edu>
2022-01-29 19:40:07 -0800

    added intern and manager functions

--- index.js
+++ index.js
@@ -32,4 +32,54 @@ function addEngineer() {
     })
 }
 
+function addIntern() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the Intern's name?",
+        name: "name",
+    }, {
+        type: "input",
+        message: "What is the Intern's email?",
+        name: "email",
+    }, {
+        type: "input",
+        message: "What is the Intern's id?",
+        name: "id",
+    }, {
+        type: "input",
+        message: "What school did the Intern go to?",
+        name: "school",
+    }]).then(function(data) {
+        console.log(data)
+        const intern = new Intern(data.name, data.email, data.id, data.school)
+        console.log(intern)
+        employees.push(intern)
+        nextPrompt()
+    })
+}
 
+function addManager() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the Manager's name?",
+        name: "name",
+    }, {
+        type: "input",
+        message: "What is the Manager's email?",
+        name: "email",
+    }, {
+        type: "input",
+        message: "What is the Manager's id?",
+        name: "id",
+    }, {
+        type: "input",
+        message: "What is the Manager's office number?",
+        name: "officeNumber",
+    }]).then(function(data) {
+        console.log(data)
+        const manager = new Manager(data.name, data.email, data.id, data.officeNumber)
+        console.log(manager)
+        employees.push(manager)
+        nextPrompt()
+    })
+}
